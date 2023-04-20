a14a60f3c5cd26a45b95d745338bdc20510c1b1a
danil tsoy <abdullaevdanil2002@gmail.com>
2023-04-20 16:52:08 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App: React.FunctionComponent = (): React.ReactElement => {
+
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+      Install & Setup Vite + React + Typescript + Tailwind CSS 3
+      </h1>
+    </div>
+  )
+}
+
+export default App
